cfd688ff7fcdf0029eef70f1d1628a8876725783
wangqiang163 <wangqiang26@jd.com>
2017-11-02 17:17:03 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author wangqiang26
+ * @mail wangqiang26@jd.com
+ * @Date 17:04 2017/11/2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("leetcode");
+    }
+}
